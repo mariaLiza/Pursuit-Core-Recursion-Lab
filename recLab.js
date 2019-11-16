8f4894356430fc7b83c3320a69697fc9dff91e60
Britt <brittanygaston@pursuit.org>
2019-11-16 17:26:35 -0500

    3/4

--- recLab.js
+++ recLab.js
@@ -34,3 +34,38 @@ const multArr = (arr, i =0, prod =1) => {
     return multArr(arr, i, prod)
 }
 console.log(multArr([5,5,1,2]))
+
+// Write a function called `concatArr` that takes 
+// in an array of strings as an argument and recursively concatenates 
+// the strings together into a single string, with spaces between each 
+// original string.
+
+// ```js
+// concatArr(['is', 'it', 'tomorrow']); // returns 'is it tomorrow'
+// concatArr(['or', 'just', 'the', 'end', 'of', 'time']); //returns '
+// or just the end of time'
+
+const concatArr = (arr, i = 0, result = "") => {
+    if(i >= arr.length) return result
+    result += arr[i] + " "
+    i++
+    return concatArr(arr,i,result)
+}
+console.log(concatArr(["or", "just", "the", "end", "of", "time"]))
+
+// Write a function called `sumEvens` that takes in an array of numbers 
+// as an argument and recursively sums only the even numbers in the array.
+
+// ```js
+// sumEvens([2, 3, 5, 6]); // returns 8
+// sumEvens([10, 5, 1, 2, 12]); //returns 24
+
+const sumEvens = (arr, i = 0, sum = 0) => {
+    if(i >= arr.length) return sum;
+    if(arr[i] % 2 === 0){
+        sum += arr[i]
+    }
+    i ++
+    return sumEvens(arr, i, sum)
+}
+console.log(sumEvens([2, 3, 5, 6]))
